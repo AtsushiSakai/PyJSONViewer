72c378d823d94043e21e463e1d7c8bc57c30705c
Atsushi Sakai <asakai.amsl+github@gmail.com>
2019-10-28 21:25:34 +0900

    change VERSION file dir

--- pyjsonviewer/pyjsonviewer.py
+++ pyjsonviewer/pyjsonviewer.py
@@ -17,7 +17,7 @@ from tkinter import font
 from tkinter import messagebox
 from urllib.parse import urlparse
 
-VERSION = open(os.path.dirname(os.path.abspath(__file__))+"/../VERSION", "r").readline()
+VERSION = open(os.path.dirname(os.path.abspath(__file__))+"/VERSION", "r").readline()
 
 # === Config ===
 MAX_N_SHOW_ITEM = 300
